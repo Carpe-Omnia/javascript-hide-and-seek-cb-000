9b2116cc91129d8195f610ac5651222369aa11c6
Tomaz Rodrigues <tomaz.r.rodrigues@gmail.com>
2019-02-19 22:49:44 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,9 +1,19 @@
 function getFirstSelector(selector) {
   return document.querySelector(selector)
 }
+
 function nestedTarget() {
   return document.querySelector('#nested .target')
 
 }
-function deepestChild() {}
-function increaseRankBy(n) {}
+
+function increaseRankBy(n) {
+  var rn = parseInt(n, 10) ;
+  
+   
+}
+
+function deepestChild() {
+  
+}
+
